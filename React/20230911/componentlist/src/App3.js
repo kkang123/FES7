4a6dfee6d1b9daf33077dc397a8f125bd6b3be84
kkang123 <one98kr@gmail.com>
2023-09-12 23:31:55 +0900

    📝 2023.09.12 React

--- React/20230911/componentlist/src/App3.js
+++ React/20230911/componentlist/src/App3.js
@@ -1,6 +1,28 @@
-import React, { useState } from 'react'
-import Login from './components/Login'
+// import React, { useState } from 'react'
+// import Login from './components/Login'
+// import Homepage from './components/Homepage';
+
+// export default function App3() {
+//     const user = {
+//         idUser: 'jaehyun@weniv.com',
+//         pwUser: 1234
+//     }
+
+//     // 로그인 상태를 판단하는 state
+//     const [login, setLogin] = useState(false);
+
+
+//     return (
+//         login ? <Homepage /> : <Login infoUser={user} setLogin={setLogin} />
+//     )
+// }
+
+// 20230912 추가
+
+import React, { useState } from 'react';
+import Login from './components/Login';
 import Homepage from './components/Homepage';
+import Modal from './components/Modal';
 
 export default function App3() {
     const user = {
@@ -10,9 +32,12 @@ export default function App3() {
 
     // 로그인 상태를 판단하는 state
     const [login, setLogin] = useState(false);
-
+    const [modalShow, setModalShow] = useState(true);
 
     return (
-        login ? <Homepage /> : <Login infoUser={user} setLogin={setLogin} />
+        <>
+            {login ? <Homepage setLogin={setLogin} /> : <Login infoUser={user} setLogin={setLogin} />}
+            {modalShow && <Modal setModalShow={setModalShow} />}
+        </>
     )
 }
