7ca4a9afd608f29b481cf9ac910844b38356d62e
kkang123 <one98kr@gmail.com>
2023-09-13 23:07:56 +0900

    📝 2023.09.13 React

--- React/20230911/modestate/src/App.js
+++ React/20230911/modestate/src/App.js
@@ -1,8 +1,20 @@
+import MenuList from "./components/MenuList/MenuList";
+import DisplayMood from "./components/DisplayMood/DisplayMood";
+import { useState } from "react";
+
 function App() {
+
+  const [currentMood, setCurrentMood] = useState("");
+
   return (
     <div>
-      hello world
+      <h1>오늘의 기분을 선택해 주세요 😄</h1>
+      <div>
+        <MenuList setCurrentMood={setCurrentMood} />
+        <DisplayMood mood={currentMood} />
+      </div>
     </div>
   );
 }
+
 export default App;
