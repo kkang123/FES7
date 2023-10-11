f2da9bbbc1a0171e2dbdbfeec7d3df425338af5d
kkang123 <one98kr@gmail.com>
2023-10-11 17:43:42 +0900

    📝 TDD

--- TDD/inputViewer/viewManager.js
+++ TDD/inputViewer/viewManager.js
@@ -1,5 +1,16 @@
 class ViewManager {
     constructor(textManager, options) {
+
+        // textManager가 TextMAnager의 인스턴스가 아니라면 
+        if (textManager.constructor !== TextManager) {
+            throw Error('textManager 객체가 아닌 다른것이 전달되었습니다.');
+        }
+
+        // options 로 전달받을 세 가지 요소중에 누락이 존재한다면
+        if (!options.inpTxt || !options.viewerEl || !options.btnEl) {
+            throw Error('options 에 필요한 요소중 누락된 요소가 존재합니다.');
+        }
+
         this.inpTxt = options.inpTxt;
         this.viewerEl = options.viewerEl;
         this.textManager = textManager;
